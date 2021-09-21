9a73b053dc9a2c80944bb3b08afefb82bc13e35f
Palak Soni <palaksoni1895@gmail.com>
2021-09-21 02:04:51 -0400

    updation

---
+++ src/Driver/Driver.java
@@ -0,0 +1,37 @@
+package Driver;
+
+import java.util.Scanner;
+
+public class Driver {
+	   public static void main(String args[]) {
+		   	  int TargetNo, TargetValue, TransactionNo, checkValue=0;
+		      System.out.println("Enter the size of transaction array :: ");
+		      Scanner s = new Scanner(System.in);
+		      int size = s.nextInt();
+		      int myArray[] = new int [size];
+		      System.out.println("Enter the values of array one by one");
+		 
+		      for(int i = 0; i<size; i++) {
+		         myArray[i] = s.nextInt();
+		     }
+		     
+		      System.out.println("Enter the total no of targets that needs to be achieved");
+		      TargetNo = s.nextInt();
+		      
+		      System.out.println("Enter the value of target");
+		      TargetValue = s.nextInt();
+		      
+		      for(int i = 0; i<TargetNo; i++) {
+		    	  checkValue = checkValue + myArray[i];
+		    	  TransactionNo = i+1;
+			         if(TargetValue<=checkValue) {
+			        	 System.out.println("Target achieved after "+TransactionNo+" transactions");
+			         }else {   
+					     System.out.println("The target is not achieved");
+			         }
+			  }
+		      
+		      
+		     
+		   }
+		}
---
+++ src/Driver/Driver2.java
@@ -0,0 +1,52 @@
+package Driver;
+
+import java.util.Arrays;   
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Driver2 {
+	 static int arraySize;
+
+	    public static void main(String args[]){
+	        System.out.println("Enter the size of currency denominations");
+	        arraySize = new Scanner(System.in).nextInt();
+
+	        int[] array =new int[arraySize];
+
+	        System.out.println("Enter the currency denominations value one by one");
+	        for(int i = 0; i< array.length ; i++){
+	        	array[i] = new Scanner(System.in).nextInt();
+	        }
+
+	        int temp;
+	        for(int i = 0 ; i < arraySize ; i++){
+	            for(int j= i+1 ; j<arraySize ; j++ ){
+	                if(array[i] < array[j]){
+	                    temp = array[i];
+	                    array[i] = array[j];
+	                    array[j] = temp;
+	                }
+	            }
+	        }
+
+	        System.out.println("Enter the amount you want to pay");
+
+	        int amount = new Scanner(System.in).nextInt();
+
+	        int[] noteCount = new int[arraySize];
+	        for(int i = 0;i< arraySize;i++){
+	            if(amount >= array[i]){
+	            	noteCount[i] = amount / array[i];
+	                amount = amount - noteCount[i] * array[i];
+	            }
+	        }
+
+	        System.out.println("Your payment approach in order to give min no of notes will be");
+
+	        for (int i = 0 ; i<arraySize ;i++){
+	            if(noteCount[i] != 0){
+	                System.out.println(array[i] + " : " + noteCount[i]);
+	            }
+	        }
+	    }
+	}
